9bf44922e4dfb7941faa02b6b741041b3be14144
hQian2718 <22kqian@athenian.org>
2021-01-11 10:40:49 +0000

    implemented two shuffles

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,16 +2,42 @@ public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) { 
-        return null;
+        int[] newArr = new int[arr.length];
+        for(int i = 0; i < arr.length - 1; i ++){
+            if(i % 2 == 0){
+                newArr[i] = arr[i / 2];
+            }else{
+                newArr[i] = arr[i / 2 + arr.length / 2];
+            }
+        }
+        newArr[arr.length - 1] = arr[arr.length - 1];
+        return newArr;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
     public static int[] selectionShuffle(int[] arr) { 
-        return null;
+        int temp;
+        int idx;
+        for(int i = 0; i < arr.length; i ++){
+            idx = (int) (Math.random() * arr.length);
+            temp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = temp;
+        }
+        return arr;
     }
 
 
     public static void main(String[] args) {
         // Write some code here to test your methods!
+        int[] arr = {};
+        arr = selectionShuffle(arr);
+        for(int i = 0; i < arr.length; i ++){
+            System.out.print(arr[i]);
+            if(i < arr.length - 1){
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
     }
 }
